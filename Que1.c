3a1838cc0d1c30960926fd334897a7477ee2279a
Durgesh Jaiswal <86183313+Jdurgesh86@users.noreply.github.com>
2023-01-14 21:58:53 +0530

    Add files via upload

---
+++ Que1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10],sum=0;
+    printf("Enter the array elements ");
+    for(int i=0;i<10;i++)
+        scanf("%d",&arr[i]);
+    for(int i=0;i<10;i++)
+        sum=sum+arr[i];
+    printf("The sum of the array is %d",sum);
+}
